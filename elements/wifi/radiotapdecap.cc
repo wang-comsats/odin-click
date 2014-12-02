ac71fce598809f409fffec94ce55f14810ab45b7
Fran Gonzalez <francisco.gonzalez.garcia@bisdn.de>
2014-12-02 16:20:28 +0100

    fixed bug in radiotapdecap.cc

--- elements/wifi/radiotapdecap.cc
+++ elements/wifi/radiotapdecap.cc
@@ -77,11 +77,11 @@ static int rt_check_header(struct ieee80211_radiotap_header *th, int len, u_int8
 
 	for (x = 0; x < NUM_RADIOTAP_ELEMENTS; x++) {
 		if (rt_el_present(th, x)) {
-
+			/*
 			int pad = bytes % radiotap_elem_to_bytes[x];
                         if (pad)
                	        	bytes += radiotap_elem_to_bytes[x] - pad;
-
+			*/
 			//Added Fran
 			if( x != 3){
 				int pad = bytes % radiotap_elem_to_bytes[x];
